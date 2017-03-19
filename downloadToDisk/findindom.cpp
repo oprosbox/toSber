a82b5f9d4f46fa52b9d58fbaabe7461b337c048a
oprosbox@yandex.ru <vovchiklen1905>
2017-03-19 17:32:47 +0300

    добавлен интерфейс

---
+++ downloadToDisk/findindom.cpp
@@ -0,0 +1,6 @@
+#include "findindom.h"
+
+findInDom::findInDom()
+{
+
+}
---
+++ downloadToDisk/findindom.h
@@ -0,0 +1,11 @@
+#ifndef FINDINDOM_H
+#define FINDINDOM_H
+
+
+class findInDom
+{
+public:
+    findInDom();
+};
+
+#endif // FINDINDOM_H
